c6a82824d7ce0f740b0a5d304353c651128a2e97
Aurélien Nicolas <info@nau.re>
2022-07-19 13:41:35 +0200

    zsa-note-type: revert output format

--- zcash_test_vectors/output.py
+++ zcash_test_vectors/output.py
@@ -14,11 +14,6 @@ def chunk(h):
 class Some(object):
     def __init__(self, thing):
        self.thing = thing
-    
-    def __eq__(self, other):
-        if other is None:
-            return False
-        return self.thing == other.thing
 
 def option(x):
     return Some(x) if x else None
@@ -202,29 +197,24 @@ def tv_part_rust(name, value, config, indent=3):
         raise ValueError('Invalid type(%s): %s' % (name, type(value)))
 
 def tv_rust(filename, parts, vectors):
-    visibility = 'pub(crate) '
-    print('// From https://github.com/zcash-hackworks/zcash-test-vectors/ (%s)' % (
+    print('        struct TestVector {')
+    for p in parts: print('            %s: %s,' % (p[0], p[1]['rust_type']))
+    print('''        };
+
+        // From https://github.com/zcash-hackworks/zcash-test-vectors/blob/master/%s.py''' % (
             filename,
         ))
-    print()
-    print(visibility + 'struct TestVector {')
-    for [name, config] in parts:
-        print('    %s%s: %s,' % (visibility, name, config['rust_type']))
-    print('}')
-    print()
     if type(vectors) == type({}):
         print('        let test_vector = TestVector {')
         for p in parts: tv_part_rust(p[0], vectors[p[0]], p[1])
         print('        };')
     elif type(vectors) == type([]):
-        print('pub(crate) fn test_vectors() -> Vec<TestVector> {')
-        print('    vec![')
+        print('        let test_vectors = vec![')
         for vector in vectors:
-            print('        TestVector {')
-            for p in parts: tv_part_rust(p[0], vector[p[0]], p[1])
-            print('        },')
-        print('    ]')
-        print('}')
+            print('            TestVector {')
+            for p in parts: tv_part_rust(p[0], vector[p[0]], p[1], 4)
+            print('            },')
+        print('        ];')
     else:
         raise ValueError('Invalid type(vectors)')
 
